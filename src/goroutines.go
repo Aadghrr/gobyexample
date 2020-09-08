3937c36013a41d98fcc019f9b427530990095420
Darragh Tobin <dtobin@dtobin-ltm1.internal.salesforce.com>
2020-09-08 22:30:30 +0100

    goroutines

---
+++ src/goroutines.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+ "fmt"
+ "time"
+)
+
+func f(from string) {
+ for i := 0; i < 3; i++ {
+  fmt.Println(from,":",i)
+ }
+}
+
+func main() {
+
+ f("in thread")
+ go f("conc")
+ go func(msg string) {
+  fmt.Println(msg)
+ }("going")
+ time.Sleep(time.Second)
+ fmt.Println("Done")
+
+}
